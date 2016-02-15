3ea9b8b010fdd85e9bab9195773373702d6a6da8
Isabella Jorissen <ifjorissen@gmail.com>
2016-02-15 12:12:45 -0800

    final commit for this morning, updated benchmark and added some more data

--- benchmark_random.sh
+++ benchmark_random.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "index, byte, size, time_ns"
+echo "size, bucket_size, time_ns checksum"
 
 # Loop over size
-for (( k = 2**8; k <= 2**30; k *= 2))
+for (( k = 2**8; k <= (2**32 -1); k *= 2))
 do
     ./sample_random $k 100000 10
 done
