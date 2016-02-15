afc52ae7c027a9ed99578a4efbb0ed5354a757f7
Isabella Jorissen <ifjorissen@gmail.com>
2016-02-15 11:54:00 -0800

    more data

--- benchmark_random.sh
+++ benchmark_random.sh
@@ -4,6 +4,6 @@ echo "index, byte, size, time_ns"
 # Loop over size
 for (( k = 2**8; k <= 2**30; k *= 2))
 do
-    ./sample_random $k 10
+    ./sample_random $k 100000 10
 done
 
--- sample_random.c
+++ sample_random.c
@@ -61,6 +61,15 @@ uint8_t random_byte(){
     return rand()%256;
 }
 
+uint32_t * generate_random_ints(size_t array_size, uint32_t bound) 
+{
+    uint32_t *arr = calloc(array_size, sizeof(uint32_t *));
+    for (uint32_t i = 0; i < array_size; i++) {
+        arr[i] = random_int(0, bound);
+    }
+    return arr;
+}
+
 uint8_t * generate_random_bytes(size_t array_size) 
 {
     uint8_t *arr = calloc(array_size, sizeof(uint8_t *));
@@ -70,19 +79,35 @@ uint8_t * generate_random_bytes(size_t array_size)
     return arr;
 }
 
-void go(uint32_t size, uint32_t iter) 
+void go(uint32_t size, uint32_t iter, uint32_t buckets) 
 {
     uint32_t seed = current_time();
     srand(seed); 
+
+    uint32_t bucket_size = (int)iter/buckets;
+    uint32_t *rand_index =  generate_random_ints(bucket_size, size);
   
     uint8_t *byte_array = generate_random_bytes(size);
-    for(uint32_t i = 0; i < iter; i ++){
-        // Start time
-        uint32_t rand_index =  random_int(0, size);
+    for(uint32_t i = 0; i < buckets; i ++){
+        uint32_t chksum = 0;
+        //generate an array w/ random indices
+
+        // if (i%bucket_size == 0){
+        free(rand_index);
+        seed = current_time();
+        srand(seed); 
+        rand_index = generate_random_ints(bucket_size, size);
+        // } 
+
         uint64_t start = current_time();
-        uint8_t b = byte_array[rand_index];
+        for (uint32_t j = 0; j < bucket_size; j++){
+            uint8_t b = byte_array[rand_index[j]];
+            chksum += b;
+        }
+        // uint64_t start = current_time();
         uint64_t end = current_time();  
-        printf("%u, %hhu, %u, %llu\n", rand_index, b, size, end - start);
+        double mean_time = (end-start)/(float)bucket_size;
+        printf("%u, %u, %f, %u\n", size, bucket_size, mean_time, chksum);
         // Print
     }
     free(byte_array);
@@ -91,14 +116,15 @@ void go(uint32_t size, uint32_t iter)
 
 int main(int argc, char **argv) 
 {
-    assert(argc == 3 && "require two arguments to run");
+    assert(argc == 4 && "require two arguments to run");
 
     uint32_t size = strtol(argv[1], NULL, 0);
     uint32_t iter = strtol(argv[2], NULL, 0);
+    uint32_t buckets = strtol(argv[3], NULL, 0);
     // uint32_t useASM = strtol(argv[3], NULL, 0);
 
-    assert(iter > 0 && size > 0 && "iter and size must be positive");
+    assert(iter > 0 && size > 0 && buckets > 0 && "iter and size must be positive");
     // assert((useASM == 0 || useASM == 1) && "Use asm should be either true or false");
 
-    go(size, iter);
+    go(size, iter, buckets);
 }
